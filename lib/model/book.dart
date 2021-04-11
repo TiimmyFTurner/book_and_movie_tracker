59847a7de8480b942cbeb109477da497c0e315c3
Arman Askari <armanaskariofficial@gmail.com>
2021-04-11 12:06:27 +0430

    crate book and movie Model

---
+++ lib/model/book.dart
@@ -0,0 +1,16 @@
+class Book {
+  final int id, pages, readTime, rate;
+  final String title, writer, note;
+  final DateTime date;
+
+  Book({
+    this.id,
+    this.title,
+    this.writer,
+    this.pages,
+    this.rate,
+    this.note,
+    this.date,
+    this.readTime,
+  });
+}
---
+++ lib/model/movie.dart
@@ -0,0 +1,17 @@
+class Mvoie {
+  final int id, time, watchTime, rate;
+  final String title, creator, favActor, note;
+  final DateTime date;
+
+  Mvoie({
+    this.id,
+    this.title,
+    this.creator,
+    this.favActor,
+    this.time,
+    this.rate,
+    this.note,
+    this.date,
+    this.watchTime,
+  });
+}
