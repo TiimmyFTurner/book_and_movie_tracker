6d0eb6e2e13953766a73df2e464a24087ecb9459
Arman Askari <armanaskariofficial@gmail.com>
2021-04-11 18:26:07 +0430

    create SplashScreen

--- lib/view/splashScreen.dart
+++ lib/view/splashScreen.dart
@@ -1,8 +1,52 @@
+import 'package:book_and_movie_tracker/view/homePage.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3)).then(
+      (value) => Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (_) => HomePage(),
+        ),
+      ),
+    );
+  }
+
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            child: Image.asset(
+              "assets/images/splash.jpg",
+              fit: BoxFit.cover,
+              height: double.infinity,
+              width: double.infinity,
+              alignment: Alignment.center,
+            ),
+          ),
+          Center(
+            child: Shimmer.fromColors(
+              baseColor: Theme.of(context).primaryColor,
+              highlightColor: Theme.of(context).accentColor,
+              child: Text(
+                "BOOK AND MOVIE\nTRACKER",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 35, fontFamily: "TrainOne"),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
