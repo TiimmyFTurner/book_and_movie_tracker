f18a32a450180e13c0dc9ad590ba01503bf0f37a
Arman Askari <armanaskariofficial@gmail.com>
2021-04-15 21:17:25 +0430

    add jalali bool for setting

--- lib/provider/settingProvider.dart
+++ lib/provider/settingProvider.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/foundation.dart';
 
-class SettingProvider extends ChangeNotifier {}
+class SettingProvider extends ChangeNotifier {
+  bool _jalali;
+  set jalali(bool value) {
+    _jalali = value;
+    notifyListeners();
+  }
+
+  get jalali => _jalali;
+}
