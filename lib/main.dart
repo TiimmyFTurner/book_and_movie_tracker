ba0a021d71a5df4d96ad0d72716e0cc52ab69007
hamidhojjati <hamidhojjati2000@gmail.com>
2021-05-01 19:16:27 +0430

    test

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false ,
       title: 'Flutter Demo',
       theme: ThemeData(
         accentColor: Color(0xff00c3ff),
