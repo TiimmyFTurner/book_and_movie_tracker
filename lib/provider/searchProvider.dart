8643ff9a14d84ea2e1758b723dca574bc4e3eec4
Arman Askari <armanaskariofficial@gmail.com>
2021-06-07 17:01:20 +0430

    compelete search page and provider

--- lib/provider/searchProvider.dart
+++ lib/provider/searchProvider.dart
@@ -4,11 +4,27 @@ import 'package:book_and_movie_tracker/model/movie.dart';
 import 'package:flutter/foundation.dart';
 
 class SearchProvider extends ChangeNotifier {
-  Future<List<Book>> serachBook(String title) {
+  List _searchItems = [];
+
+  List get searchItems => _searchItems;
+
+  void search(String search) {
+    _searchItems = [];
+    searchBook(search).then((value) {
+      if (value != null || value != []) _searchItems += value;
+      searchMovie(search).then((value) {
+        if (value != null || value != []) _searchItems += value;
+        _searchItems.shuffle();
+        notifyListeners();
+      });
+    });
+  }
+
+  Future<List<Book>> searchBook(String title) {
     return DBProvider.db.searchBook(title);
   }
 
-  Future<List<Movie>> serachMovie(String title) {
+  Future<List<Movie>> searchMovie(String title) {
     return DBProvider.db.searchMovie(title);
   }
 }
--- lib/view/search.dart
+++ lib/view/search.dart
@@ -1,6 +1,17 @@
+import 'package:book_and_movie_tracker/model/movie.dart';
+import 'package:book_and_movie_tracker/widget/bookCard.dart';
+import 'package:book_and_movie_tracker/widget/movieCard.dart';
 import 'package:flutter/material.dart';
+import 'package:book_and_movie_tracker/provider/providers.dart';
 
-class Search extends StatelessWidget {
+class Search extends StatefulWidget {
+  @override
+  _SearchState createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  List<dynamic> _items = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,50 +21,69 @@ class Search extends StatelessWidget {
           style: TextStyle(color: Colors.black),
         ),
         centerTitle: true,
-        leading: Icon(Icons.settings, color: Colors.red,),
+        leading: IconButton(
+            icon: Icon(Icons.settings, color: Theme.of(context).primaryColor),
+            onPressed: () {}),
         elevation: 0,
-        backgroundColor: Colors.white,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         actions: [
           IconButton(
-            icon: Icon(
-              Icons.home_outlined,
-              color: Colors.red,),
-          ),
+              icon: Icon(Icons.home_outlined,
+                  color: Theme.of(context).primaryColor),
+              onPressed: () {}),
         ],
       ),
       body: Directionality(
         textDirection: TextDirection.ltr,
-        child: Stack(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Container(
-              child: Padding(padding: EdgeInsets.only(top: 5),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Material(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'جستجو',
-                            icon: Padding(
-
-                              padding: EdgeInsets.only(left: 15),
-                              child: Icon(Icons.search, color: Colors.red,),
-                            ),
-                          ),
-                          textAlign: TextAlign.right,
-                        ),
-                        elevation: 20,
-                        borderRadius: BorderRadius.circular(20),
-                        shadowColor: Colors.grey[200],
+            Padding(
+              padding: EdgeInsets.all(6),
+              child: Material(
+                elevation: 8,
+                borderRadius: BorderRadius.circular(9),
+                shadowColor: Colors.grey[200],
+                child: TextField(
+                  textAlign: TextAlign.right,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                    border: InputBorder.none,
+                    hintText: 'جستجو',
+                    icon: Padding(
+                      padding: EdgeInsets.only(left: 12),
+                      child: Icon(
+                        Icons.search,
+                        color: Theme.of(context).primaryColor,
                       ),
-                    ]
+                    ),
+                  ),
+                  onChanged: (value){
+                    context.read<SearchProvider>().search(value);
+                  },
                 ),
               ),
             ),
+            Expanded(child: _movieList(context))
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _movieList(BuildContext context) {
+    _items = context.watch<SearchProvider>().searchItems;
+    return Container(
+      child: ListView.builder(
+        physics: BouncingScrollPhysics(),
+        padding: const EdgeInsets.all(2),
+        itemCount: _items.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _items[index].runtimeType == Movie
+              ? MovieCard(_items[index])
+              : BookCard(_items[index]);
+        },
       ),
     );
   }
